d51632f3182715c3d73767c3f1f6d91428cc3874
InterferenceObject <admin@ot-sdt-serv.com>
2020-10-25 13:59:37 +0200

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Boost Error Plugin
+This plugin is designed to add Google Cloud Secret Manager integration to allow authorization to Sanctum in Google Cloud environments
+
+# Installation
+```sh
+npm install --save github.com:epicdev-za/boost-gc-sanctum-authentictor
+```
+
+Register it in **server.config.js**
+```js
+...
+sanctum: {
+    project_key: '',
+    gc_project_id: ''//Insert Google Cloud Project ID
+},
+plugins: ['boost-gc-sanctum-authentictor'], //Or use boost-error-plugin/gc for Google Cloud Error Reporting
+endpoints: {
+...
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+const BoostPlugin = require("boost/api/plugins/BoostPlugin");
+const crypto = require("crypto");
+const {SecretManagerServiceClient} = require("@google-cloud/secret-manager");
+const client = new SecretManagerServiceClient();
+
+class GCSanctumAuthenticator extends BoostPlugin{
+
+    async onSanctumEncrypt(data, project_key) {
+        const config = require("../../server.config");
+        const gc_project_id = (config.sanctum.gc_project_id !== undefined) ? config.sanctum.gc_project_id : null;
+        if(gc_project_id !== null){
+            let version = "projects/" + gc_project_id + "/secrets/sanctum-public/versions/latest";
+
+            const [version] = await client.accessSecretVersion({
+                name: version
+            });
+
+            const key = version.payload.data.toString();
+
+            try{
+                return crypto.publicEncrypt({
+                    key: key,
+                    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                    oaepHash: "sha256"
+                }, Buffer.from(JSON.stringify(data))).toString("base64");
+            }catch (e){
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    async onSanctumDecrypt(data, project_key) {
+        const config = require("../../server.config");
+        const gc_project_id = (config.sanctum.gc_project_id !== undefined) ? config.sanctum.gc_project_id : null;
+        if(gc_project_id !== null) {
+            let version = "projects/" + gc_project_id + "/secrets/sanctum-private/versions/latest";
+
+            const [version] = await client.accessSecretVersion({
+                name: version
+            });
+
+            const key = version.payload.data.toString();
+
+            try {
+                return JSON.parse(crypto.privateEncrypt({
+                    key: key,
+                    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                    oaepHash: "sha256"
+                }, Buffer.from(data, "base64")).toString());
+            } catch (e) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+}
