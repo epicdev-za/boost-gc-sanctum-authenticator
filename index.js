98c5c2886069740640fb44bc1a8a69d3d123dc57
InterferenceObject <admin@ot-sdt-serv.com>
2020-10-27 09:38:04 +0200

    WIP

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new SecretManagerServiceClient();
 
 class GCSanctumAuthenticator extends BoostPlugin{
 
-    async onSanctumEncrypt(data, project_key) {
+    async onSanctumEncryptSecret(data, project_key) {
         const config = require("../../server.config");
         const gc_project_id = (config.sanctum.gc_project_id !== undefined) ? config.sanctum.gc_project_id : null;
         if(gc_project_id !== null){
@@ -24,32 +24,6 @@ class GCSanctumAuthenticator extends BoostPlugin{
                     oaepHash: "sha256"
                 }, Buffer.from(JSON.stringify(data))).toString("base64");
             }catch (e){
-                return null;
-            }
-        }
-
-        return null;
-    }
-
-    async onSanctumDecrypt(data, project_key) {
-        const config = require("../../server.config");
-        const gc_project_id = (config.sanctum.gc_project_id !== undefined) ? config.sanctum.gc_project_id : null;
-        if(gc_project_id !== null) {
-            let name = "projects/" + gc_project_id + "/secrets/sanctum-private/versions/latest";
-
-            const [version] = await client.accessSecretVersion({
-                name: name
-            });
-
-            const key = version.payload.data.toString();
-
-            try {
-                return JSON.parse(crypto.privateDecrypt({
-                    key: key,
-                    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-                    oaepHash: "sha256"
-                }, Buffer.from(data, "base64")).toString());
-            } catch (e) {
                 return null;
             }
         }
